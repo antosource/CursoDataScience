e2df192e71543a144be3ca441516f1a33e9f0e08
Antonio Chávez V <102922148+antosource@users.noreply.github.com>
2022-05-07 12:34:08 -0400

    Delete 2 directory

--- 2/emprendedor1.py
+++
@@ -1,9 +0,0 @@
-try:
-    p = int(input('Ingrese el precio de suscripción: '))
-    u = int(input('Ingrese el numero de usuarios: '))
-    gt = int(input('Ingrese gasto total: '))
-    u = p*u -gt
-    print(f'La utilidad es : {u}')
-except Exception:
-    print('Ingrese un numero entero')
-    
--- 2/emprendedor2.py
+++
@@ -1,9 +0,0 @@
-try:
-    p = int(input('Ingrese el precio de suscripción: '))
-    un = int(input('Ingrese el numero de usuarios normales: '))
-    up = int(input('Ingrese el numero de usuarios premium: '))
-    gt = int(input('Ingrese gasto total: '))
-    u = p*(un+up*1.5) -gt
-    print(f'La utilidad es : {u:.2f}')
-except Exception:
-    print('Ingrese un numero entero')
