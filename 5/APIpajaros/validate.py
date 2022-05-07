34937d8e60c386e88cfcb60026dc75fe5fcbb301
Antonio Chávez V <102922148+antosource@users.noreply.github.com>
2022-05-07 12:35:04 -0400

    Delete 5 directory

--- 5/APIpajaros/validate.py
+++
@@ -1,6 +0,0 @@
-def validate(valor):
-    while type(valor)!=int:
-        try:
-            return int(valor)
-        except:
-            valor = input("Ingrese valor valido: ")
--- 5/APIpajaros/validate_2.py
+++
@@ -1,6 +0,0 @@
-def validate(valor):
-    try:
-        return int(valor)
-    except:
-        valor = input("Ingrese valor valido: ")
-        return(validate(valor))
