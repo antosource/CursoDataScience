26fdd139c06371b54c9e7b78c8323109fe18d770
Antonio Chávez V <antoniochavezvasquez@gmail.com>
2022-06-16 09:21:29 -0400

    temporal

--- IntroduccionPython/5/API_ejemplo_response.py
+++ IntroduccionPython/5/API_ejemplo_response.py
@@ -1,11 +1,22 @@
-
 import requests
+import json
 
-url = 'https://jsonplaceholder.typicode.com/posts'
-payload = {}
-headers = {}
+#verificar token
 
-response = requests.request('GET', url, headers=headers, data=payload)
-print (response)
-print (payload)
-print (headers)
+#autentificar
+
+#publicar
+
+
+
+def reqPost(url, json={}):
+   response=requests.request (url=url, data=data)
+   return json.loads(response.text)
+
+
+
+url = 'https://conexiones.sma.gob.cl/api/v1/'
+auth_data = {"usuario":"96989120-4","password":"QI}|3)g_0)"}
+
+resp = requests.post(url+'auth', json=auth_data)
+print (resp.json())
